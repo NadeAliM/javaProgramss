bec266e06b459d5fd72bf0bc892ca4b423b17aa0
Rafeek313 <epyshan@gmail.com>
2022-04-18 15:12:54 +0530

    reverse string program added

---
+++ src/String/ReverseAstring1.java
@@ -0,0 +1,12 @@
+package String;
+
+public class ReverseAstring1 {
+
+	public static void main(String[] args) {
+		String s="TestYantra";
+		StringBuffer sb=new StringBuffer(s);
+		System.out.println(sb.reverse());
+
+	}
+
+}
